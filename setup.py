6cb9617da183fdbf30c5b0d1d967cfc37d62be6c
Patric Stout <truebrain@openttd.org>
2020-11-11 10:04:17 +0100

    Add: change the license to LGPLv3
    As wikitextparser is licensed under GPLv3, this library cannot
    be licensed under LGPLv2 (according to the FAQ on the GNU website).
    According to that same FAQ, this library can be licensed under
    LGPLv3, hence the change of license.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.8',
