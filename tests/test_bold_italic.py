969e2f8f7c4b6e231e03a7f511368291e7209f79
Patric Stout <github@truebrain.nl>
2023-06-07 11:11:33 +0200

    Fix: overlapping italic / bold could case exception

--- tests/test_bold_italic.py
+++ tests/test_bold_italic.py
@@ -10,3 +10,14 @@ def test_bold_and_italic():
             return True
 
     assert PageTest("Test").render().html == "<p><i>i</i> - <b>b</b> - <i><b>ib</b></i></p>"
+
+
+def test_overlapping_bold_and_italic():
+    class PageTest(Page):
+        def page_load(self, page: str) -> str:
+            return "''a''' ''b'' zz'''c''"
+
+        def page_exists(self, page: str) -> bool:
+            return True
+
+    assert PageTest("Test").render().html == "<i>a<b></i>b<i>zz</b>c</i>"
