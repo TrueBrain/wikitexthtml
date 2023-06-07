8e5c10cfa7b771364dc53b6d841cc8817c404b3d
Patric Stout <truebrain@openttd.org>
2023-06-07 11:36:10 +0200

    Fix: overlapping italic / bold could case exception (#34)

--- wikitexthtml/render/bold_and_italic.py
+++ wikitexthtml/render/bold_and_italic.py
@@ -5,7 +5,13 @@ from ..prototype import WikiTextHtml
 
 def replace(instance: WikiTextHtml, wikitext: wikitextparser.WikiText):
     for bold in reversed(wikitext.get_bolds(recursive=True)):
-        bold.string = f"<b>{bold.text.strip()}</b>"
+        # Replace the closing tag with a placeholder, of the exact same length
+        # as the original text ('''). This makes sure the italic tags are still
+        # in the same place.
+        bold.string = f"<b>{bold.text.strip()}<B>"
 
     for italic in reversed(wikitext.get_italics(recursive=True)):
         italic.string = f"<i>{italic.text.strip()}</i>"
+
+    # Replace the placeholders with the closing tags.
+    wikitext.string = wikitext.string.replace("<B>", "</b>")
